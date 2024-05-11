9118e55114dff4dc308a514f0638446ce5515175
Aleksandr <aleksandritecoman@gmail.com>
2024-05-11 13:36:42 +0300

    Task 16 is ready

---
+++ src/main/java/com/iteco/multithreading/Main.java
@@ -0,0 +1,35 @@
+package com.iteco.multithreading;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+    public static void main(String[] args) {
+        // > 4 person
+        Road road = new Road();
+        Thread cars = new Thread(() -> {
+            try {
+                road.startCars();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "cars");
+
+        Thread person = new Thread(() -> {
+            try {
+                road.startPerson();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "person");
+
+        person.start();
+        cars.start();
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.exit(0);
+    }
+}
